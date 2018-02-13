8ea117c3bde8a249d4f1bf7f70bff84dd159006a
jm <johnmichaelmaytona@gmail.com>
2018-02-12 23:59:32 -0800

    Added Request Page using PHP

---
+++ request-page.php
@@ -0,0 +1,152 @@
+<?php
+
+    //Message Variable
+    $msg = '';
+    $msgClass = '';
+
+    //Submit
+    if(filter_has_var(INPUT_POST, 'submit')){
+        //Get Form Data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $order = $_POST['order'];
+        $message = $_POST['message'];
+        
+        //Check Required Field
+        if(!empty($name) && !empty($email) && !empty($order) && !empty($message)){
+            //Proceed
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)=== false){
+                //Failed
+                $msg = 'Please use a valid email';
+                $msgClass = 'alert-danger';
+            } else {
+                //Proceed
+                $toEmail = 'johnmichaelaytona@gmail.com';
+                $subject = 'Order Request From '.$name;
+                $body = '<h2>Contact Request </h2>
+                <h4>Name<h4><p>'.$name.'</p>
+                <h4>Email<h4><p>'.$email.'</p>
+                <h4>Order<h4><p>'.$order.'</p>
+                <h4>Mesesage<h4><p>'.$message.'</p>
+                ';
+                
+                //Email Headers
+                $headers = "MIME-Version: 1.0" ."\r\n";
+                $headers .= "Content-Type:text/html; charset=UTF-8" ."\r\n";
+                
+                //Additional Headers(from)
+                $headers .= "From: ". $name."<". $email. ">". "\r\n";
+                
+                if(mail($toEmail, $subject, $body, $headers)){
+                    //Email Sent
+                    $msg = 'Your email has been sent';
+                    $msgClass = 'alert-success';
+                } else {
+                    $msg = 'Your email was not sent';
+                    $msgClass = 'alert-danger';
+                }
+                
+            }
+        } else {
+            //Failed
+            $msg = 'Please fill in all fields';
+            $msgClass = 'alert-danger';
+        }
+       
+        
+    }
+?>
+
+
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Request Form</title>
+    </head>
+
+    <body>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img src="assets/logo2.png"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Catering</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Offers</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Packages</a>
+                            <a class="dropdown-item" href="#">Deserts and Pastries</a>
+                            <a class="dropdown-item" href="#">Designs</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+
+        <div class="container">
+            <?php if($msg != ''): ?>
+            <div class="alert <?php echo $msgClass; ?>">
+                <?php echo $msg; ?>
+            </div>
+            <?php endif; ?>
+
+            <h1>Request Order</h1>
+
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <div class="form-group">
+                    <label for="formGroupName">Full Name</label>
+                    <input type="text" name="name" class="form-control" id="nameID" placeholder="Name" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupOrder">Email</label>
+                    <input type="text" name="email" class="form-control" id="emailID" placeholder="Email" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupOrder">Order Package</label>
+                    <input type="text" name="order" class="form-control" id="orderID" placeholder="Order" value="<?php echo isset($_POST['order']) ? $order : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupPackage">Message</label>
+                    <textarea type="text" name="message" class="form-control" id="messageID"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+
+
+        </div>
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
